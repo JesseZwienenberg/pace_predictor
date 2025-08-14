2519d1f891444f008968d9eb474a50bf9f203162
= <jessez1997@gmail.com>
2025-08-14 09:10:49 +0200

    Refactored to Running Experts, added information to dashboard, changed titles of pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    @activities = Activity.order(start_date: :desc)
+    @total_distance = Activity.sum(:distance) / 1000.0  # Convert to km
+    @total_time = Activity.sum(:duration)  # In seconds
+    @avg_pace = @activities.any? ? @activities.average("duration::float / (distance / 1000.0) / 60.0") : 0
   end
 end
